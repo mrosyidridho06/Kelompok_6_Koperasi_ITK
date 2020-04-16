d81f26b82522eaa3ff09ced5fe897c1723701474
Anggara Azis Saputra <61194516+Anggarazs@users.noreply.github.com>
2020-04-16 12:59:38 +0800

    Update simpanpinjam.py

--- simpanpinjam.py
+++ simpanpinjam.py
@@ -1,32 +1,33 @@
 class Simpanpinjam:
     def __init__(self):
-        self.id_nasabah = ""
-        self.tanggal = ""
-        self.jumlah_simpan = ""
-        self.jumlah_pinjam = ""
+        self.__id_nasabah = ""
+        self.__tanggal = ""
+        self.__jumlah_simpan = ""
+        self.__jumlah_pinjam = ""
 
     def getId_nasabah(self):
-        return "Id_nasabah : {}".format(self.id_nasabah)
+        return "Id_nasabah : {}".format(self.__id_nasabah)
 
     def setId_nasabah(self, id):
-        self.id_nasabah = id
+        self.__id_nasabah = id
 
     def getTanggal(self):
-        return "Tanggal : {}".format(self.tanggal)
+        return "Tanggal : {}".format(self.__tanggal)
 
     def setTanggal(self, tanggal):
-        self.tanggal = tanggal
+        self.__tanggal = tanggal
 
     def getJumlah_simpan(self):
-        return "Jumlah_simpan : {}".format(self.jumlah_simpan)
+        return "Jumlah_simpan : {}".format(self.__jumlah_simpan)
+    
     def setJumlah_simpan(self, jumlah_simpan):
-        self.jumlah_simpan = jumlah_simpan
+        self.__jumlah_simpan = jumlah_simpan
 
     def getJumlah_pinjam(self):
-        return "Jumlah_pinjam : {}".format(self.jumlah_pinjam)
+        return "Jumlah_pinjam : {}".format(self.__jumlah_pinjam)
 
     def setJumlah_pinjam(self, jumlah_pinjam):
-        self.jumlah_pinjam = jumlah_pinjam
+        self.__jumlah_pinjam = jumlah_pinjam
 
 sp = Simpanpinjam()
 sp.setJumlah_simpan(input("Masukkan jumlah simpanan : Rp."))
