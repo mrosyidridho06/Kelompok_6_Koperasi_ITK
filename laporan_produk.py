204ad3ac203acd9c4177ea16469ed53191ecd4c2
akbar-sys <61173163+akbar-sys@users.noreply.github.com>
2020-04-16 12:01:24 +0800

    Add files via upload

---
+++ laporan_produk.py
@@ -0,0 +1,41 @@
+class Laporan_produk:
+    def __init__(self):
+        self.id_barang = ""
+        self.harga_produk = ""
+        self.tanggal = ""
+        self.jumlah_produk = ""
+        self.nama_produk = ""
+
+    def getId_barang(self):
+        return "ID Barang : {}".format(self.id_barang)
+
+    def setId_barang(self, id):
+        self.id_barang = id
+
+    def getHarga_produk(self):
+        return "Harga Produk : {}".format(self.harga_produk)
+
+    def setHarga_produk(self, harga):
+        self.harga_produk = harga
+
+    def getTanggal(self):
+        return "Tanggal : {}".format(self.tanggal)
+
+    def setTanggal(self, tanggal):
+        self.tanggal = tanggal
+
+    def getJumlah_produk(self):
+        return "Jumlah Produk : {}".format(self.jumlah_produk)
+
+    def setJumlah_produk(self, jumlah):
+        self.jumlah_produk = jumlah
+
+    def getNama_produk(self):
+        return "Nama Produk: {}".format(self.nama_produk)
+
+    def setNama_produk(self, nama):
+        self.nama_produk = nama
+
+lapprod = Laporan_produk()
+lapprod.setId_barang(input("masukkan id barang : "))
+print(lapprod.setId_barang())
---
+++ transaksi_produk.py
@@ -0,0 +1,27 @@
+class Transaksi_produk:
+    def __init__(self):
+        self.idPembelian = ""
+        self.tanggalTransaksi = ""
+        self.totalPembayaran = ""
+
+    def getIdPembelian(self):
+        return "ID Pembelian : {}".format(self.idPembelian)
+
+    def SetIdPembelian(self, id):
+        self.idPembelian = id
+
+    def getTanggalTransaksi(self):
+        return "Tanggal Transaksi : {}".format(self.tanggalTransaksi)
+
+    def setTanggalTransaksi(self, tanggal):
+        self.tanggalTransaksi = tanggal
+
+    def getTotalPembayaran(self):
+        return "Total Pembayaran: {}".format(self.totalPembayaran)
+
+    def setTotalPembayaran(self, total):
+        self.totalPembayaran = total
+
+transprod = Transaksi_produk
+transprod.setTanggalTransaksi(input("masukkan tanggal transaksi : "))
+print(transprod.setTanggalTransaksi())
