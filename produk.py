4a23a52c027469fc4ec409052ae5c9965b206423
akbar-sys <61173163+akbar-sys@users.noreply.github.com>
2020-04-16 13:15:00 +0800

    Update produk.py

--- produk.py
+++ produk.py
@@ -1,40 +1,40 @@
 class Produk:
     def __init__(self):
-        self.id_barang = ""
-        self.nama_produk = ""
-        self.jumlah_produk = ""
-        self.harga_produk = ""
-        self.tanggal_masuk = ""
+        self.__id_barang = ""
+        self.__nama_produk = ""
+        self.__jumlah_produk = ""
+        self.__harga_produk = ""
+        self.__tanggal_masuk = ""
 
     def getId_barang(self):
-        return "ID Barang : {}".format(self.id_barang)
+        return "ID Barang : {}".format(self.__id_barang)
 
     def SetId_barang(self, id):
-        self.id_barang = id
+        self.__id_barang = id
 
     def getNama_produk(self):
-        return "Nama Produk: {}".format(self.nama_produk)
+        return "Nama Produk: {}".format(self.__nama_produk)
 
     def setNama_produk(self, nama):
-        self.nama_produk = nama
+        self.__nama_produk = nama
 
     def getJumlah_produk(self):
-        return "Jumlah Produk : {}".format(self.jumlah_produk)
+        return "Jumlah Produk : {}".format(self.__jumlah_produk)
 
     def setJumlah_produk(self, jumlah):
-        self.jumlah_produk = jumlah
+        self.__jumlah_produk = jumlah
 
     def getHarga_produk(self):
-        return "Harga Produk : {}".format(self.harga_produk)
+        return "Harga Produk : {}".format(self.__harga_produk)
 
     def setHarga_produk(self, harga):
-        self.harga_produk = harga
+        self.__harga_produk = harga
 
     def getTanggal_masuk(self):
-        return "Tanggal Masuk : {}".format(self.tanggal_masuk)
+        return "Tanggal Masuk : {}".format(self.__tanggal_masuk)
 
     def setTanggal(self, tanggal):
-        self.tanggal = tanggal
+        self.__tanggal = tanggal
 
 prod = Produk()
 prod.setNama_produk(input("masukkan nama produk : "))
