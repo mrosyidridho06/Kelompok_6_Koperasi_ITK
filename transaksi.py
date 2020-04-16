1ad0d9c7e9dcadecd817e3fed93a0f0b406cd1d6
Anggara Azis Saputra <61194516+Anggarazs@users.noreply.github.com>
2020-04-16 13:00:39 +0800

    Update transaksi.py

--- transaksi.py
+++ transaksi.py
@@ -1,19 +1,19 @@
 class Transaksi:
     def __init__(self):
-        self.id_nasabah = ""
-        self.tgl_transaksi = ""
+        self.__id_nasabah = ""
+        self.__tgl_transaksi = ""
 
     def getId_nasabah(self):
-        return "Id_nasabah : {}".format(self.id_nasabah)
+        return "Id_nasabah : {}".format(self.__id_nasabah)
 
     def setId_nasabah(self, id):
-        self.id_nasabah = id
+        self.__id_nasabah = id
 
     def getTgl_transaksi(self):
-        return "Tanggal Transaksi : {}".format(self.tgl_transaksi)
+        return "Tanggal Transaksi : {}".format(self.__tgl_transaksi)
 
     def setTgl_transaksi(self, tgl):
-        self.tgl_transaksi = tgl
+        self.__tgl_transaksi = tgl
 
 
 sp = Transaksi()
