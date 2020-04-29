da5f0c358585e55848b81c6c2eb2b88ce2d76a3b
akbar-sys <61173163+akbar-sys@users.noreply.github.com>
2020-04-29 11:26:29 +0800

    UPDATE CRUD ORM

--- supplier.py
+++ supplier.py
@@ -1,3 +1,6 @@
+from base import sessionFactory
+from SupplierORM import SupplierORM
+
 class supplier:
     def __init__(self, id_supplier, nama_supplier, no_telp, alamat_supplier):
         self.__id_supplier = id_supplier
@@ -37,7 +40,64 @@ class supplier:
     def alamat_supplier(self, alamat):
         self.__alamat_supplier = alamat
 
-a = supplier(1,"erza",41312,"Ruby")
-print(a.nama_supplier)
-a.nama_supplier = "fasya"
-print(a.nama_supplier)
+        
+    def insertSupplier(self):
+            try:
+                session = sessionFactory()
+                supplier = SupplierORM(self.__nama_supplier, self.__no_telp, self.__alamat_supplier)
+                session.add(supplier)
+                session.commit()
+                session.close()
+            except Exception as e:
+                print("===>", e)
+            else:
+                print("Data telah Disimpan!")
+                
+    @staticmethod
+    def DataSupplier():
+        try:
+            session = sessionFactory()
+            for Supplier in session.query(SupplierORM).all():
+                print(
+                    "Id supplier = {}\nNama = {}\nNo Telp = {}\nAlamat = {}\n===================="
+                        .format(supplier.id_supplier, supplier.nama_supplier, supplier.no_telp, supplier.alamat_supplier))
+            session.close()
+        except Exception as e:
+            print("===>", e)
+            
+    @staticmethod
+    def deleteSupplier(id_supplier):
+        try:
+            session = sessionFactory()
+            session.query(SupplierORM).filter_by(id_supplier=id_supplier).delete()
+            session.commit()
+            session.close()
+        except Exception as e:
+            print("===>", e)
+        else:
+            print("Data terlah terhapus!")
+            
+    @staticmethod
+    def updateSupplier(id_Supplier):
+        try:
+            newNama = input("Nama Supplier : ")
+            newNotelp = input("No Telp Supplier: ")
+            newAlamat = input("Alamat Supplier : ")
+            session = sessionFactory()
+            session.query(SupplierORM).filter_by(id_supplier=id_supplier).update({
+                SupplierORM.nama_supplier: newNama, SupplierORM.no_telp: newNotelp,
+                SupplierORM.alamat_supplier: newAlamat
+            }, synchronize_session=False)
+            session.commit()
+            session.close()
+        except Exception as e:
+            print("===>", e)
+        else:
+            print("Data telah Terupdate!")
+            
+s = Supplier("Joni", "0881238472", "jl.Ruqyah")
+#print(s)
+s.insertSupplier()
+#Supplier.deleteSupplier(2)
+#Supplier.updateSupplier(2)
+supplier.DataSupplier()
