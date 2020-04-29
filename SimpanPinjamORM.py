c16c910fa4105206136fe0474c90835bd1af2f60
akbar-sys <61173163+akbar-sys@users.noreply.github.com>
2020-04-29 11:51:17 +0800

    Update SimpanPinjamORM.py

--- SimpanPinjamORM.py
+++ SimpanPinjamORM.py
@@ -15,9 +15,9 @@ class SimpanPinjamORM(base):
     jumlah_pinjam = Column(Integer)
 
     def __init__(self, id_nasabah, tanggal, jumlah_simpan, jumlah_pinjam):
-        self.__id_nasabah= id_nasabah
-        self.__tanggal = tanggal
-        self.__jumlah_simpan = jumlah_simpan
-        self.__jumlah_pinjam = jumlah_pinjam
+        self.id_nasabah= id_nasabah
+        self.tanggal = tanggal
+        self.jumlah_simpan = jumlah_simpan
+        self.jumlah_pinjam = jumlah_pinjam
         
 #sessionFactory()
