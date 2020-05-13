eabf1a3cedf776a86f7e8c6f18d20f5dab8938ea
akbar-sys <61173163+akbar-sys@users.noreply.github.com>
2020-05-14 02:03:51 +0800

    Add files via upload

---
+++ view/simpanPinjam.py
@@ -0,0 +1,97 @@
+from PyQt5.QtWidgets import QApplication, QPushButton, QLineEdit, QVBoxLayout,  QSpinBox, QLabel, QFormLayout,  QGroupBox, QDateEdit ,QDialog, QTabWidget, QWidget
+from PyQt5 import QtGui
+from PyQt5 import QtCore
+import sys
+
+class Tab(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('Tab Widget')
+
+        vbox = QVBoxLayout()
+        tabWidget = QTabWidget()
+
+        tabWidget.addTab(inputSimpan(), 'simpan')
+        tabWidget.addTab(inputPinjam(), 'pinjam')
+
+        vbox.addWidget(tabWidget)
+
+        self.setLayout(vbox)
+
+class inputSimpan(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.createFormGroupBox()
+
+        title = "Koperasi ITK"
+        left = 0
+        right = 0
+        width = 400
+        height = 600
+        iconName = "img/icon.png"
+
+        mainLayout = QVBoxLayout()
+        mainLayout.addWidget(self.formGroupBox)
+
+        submitBtn = QPushButton("Submit")
+
+        mainLayout.addWidget(submitBtn)
+
+        self.setWindowTitle(title)
+        self.setWindowIcon(QtGui.QIcon(iconName))
+        self.setGeometry(left, right, height, width)
+        self.setLayout(mainLayout)
+
+        self.show()
+
+    def createFormGroupBox(self):
+        self.formGroupBox = QGroupBox()
+        self.formGroupBox.setAlignment(QtCore.Qt.AlignCenter)
+        layout = QFormLayout()
+        layout.addRow(QLabel("Nama Nasabah :"), QLineEdit())
+        layout.addRow(QLabel("Tanggal :"), QDateEdit())
+        layout.addRow(QLabel("Jumlah Simpan:"), QSpinBox())
+        self.formGroupBox.setLayout(layout)
+
+
+class inputPinjam(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.createFormGroupBox()
+
+        title = "Koperasi ITK"
+        left = 0
+        right = 0
+        width = 400
+        height = 600
+        iconName = "img/icon.png"
+
+        mainLayout = QVBoxLayout()
+        mainLayout.addWidget(self.formGroupBox)
+
+        submitBtn = QPushButton("Submit")
+
+        mainLayout.addWidget(submitBtn)
+
+        self.setWindowTitle(title)
+        self.setWindowIcon(QtGui.QIcon(iconName))
+        self.setGeometry(left, right, height, width)
+        self.setLayout(mainLayout)
+
+        self.show()
+
+    def createFormGroupBox(self):
+        self.formGroupBox = QGroupBox()
+        self.formGroupBox.setAlignment(QtCore.Qt.AlignCenter)
+        layout = QFormLayout()
+        layout.addRow(QLabel("Nama Nasabah :"), QLineEdit())
+        layout.addRow(QLabel("Tanggal :"), QDateEdit())
+        layout.addRow(QLabel("Jumlah Pinjam:"), QSpinBox())
+        self.formGroupBox.setLayout(layout)
+
+if __name__ == "__main__":
+    App = QApplication(sys.argv)
+    tabDialog = Tab()
+    tabDialog.show()
+    App.exec()
